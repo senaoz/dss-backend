4fc16e21f6bc1a3136d1b0aab354ae8670cfe12c
Sena Oz <tazeyta@gmail.com>
2023-12-15 21:08:46 +0300

    add model

--- src/api/model/controllers/model.js
+++ src/api/model/controllers/model.js
@@ -1,10 +1,40 @@
+"use strict";
+
+/**
+ * A set of functions called "actions" for `model`
+ */
+
 module.exports = {
-  model: async (ctx) => {
-    const input = ctx.request.body;
-    const inputFeatures = Object.values(input).join(" ");
+  model: async (ctx, next) => {
+    try {
+      const input = ctx.request.body;
+      const inputFeatures = Object.values(input).join(" ");
 
-    const response = await strapi.services.model.predictPrice(inputFeatures);
+      const { exec } = require("child_process");
 
-    ctx.body = response;
+      const result = new Promise((resolve, reject) => {
+        exec(
+          `python src/api/model/services/model.py ${inputFeatures}`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.log(`error: ${error.message}`);
+              reject(error.message);
+            }
+            if (stderr) {
+              console.log(`stderr: ${stderr}`);
+              reject(stderr);
+            }
+            console.log(`stdout: ${stdout}`);
+            resolve(stdout);
+          }
+        );
+      });
+
+      const response = await result;
+
+      ctx.body = response;
+    } catch (err) {
+      ctx.body = err;
+    }
   },
 };
